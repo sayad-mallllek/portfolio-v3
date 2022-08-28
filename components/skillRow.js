3aa2078a6501b67b9328a1c95bc4d9105adaec0b
sayad-mallllek <bob@khalilelzein.com>
2022-08-28 16:01:59 +0300

    added some tech in skills page

---
+++ components/skillRow.js
@@ -0,0 +1,95 @@
+import { useState } from 'react'
+import {
+  Container,
+  GridItem,
+  Text,
+  Progress,
+  SlideFade,
+  Collapse,
+  Box,
+  useMediaQuery
+} from '@chakra-ui/react'
+import { BiChevronDown } from 'react-icons/bi'
+
+const SkillRow = ({ icon, progressValue, progressColor, text }) => {
+  const [rotated, setRotated] = useState(false)
+  const isMobile = useMediaQuery('(min-width: 600px)')
+
+  const handleSetRotated = () => {
+    setRotated(!rotated)
+  }
+  return (
+    <>
+      <GridItem
+        colSpan={1}
+        onClick={handleSetRotated}
+        sx={{
+          cursor: 'pointer'
+        }}
+      >
+        <Container
+          height="100%"
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center'
+          }}
+        >
+          {icon}
+        </Container>
+      </GridItem>
+      <GridItem
+        colSpan={5}
+        onClick={handleSetRotated}
+        sx={{
+          cursor: 'pointer'
+        }}
+      >
+        <Container
+          width="100%"
+          height="100%"
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center'
+          }}
+        >
+          <Progress
+            value={progressValue}
+            size="sm"
+            colorScheme={progressColor}
+            borderRadius={4}
+            transitionDuration={1}
+          />
+        </Container>
+      </GridItem>
+      <GridItem
+        colSpan={1}
+        onClick={handleSetRotated}
+        sx={{
+          cursor: 'pointer'
+        }}
+      >
+        <BiChevronDown
+          fontSize={40}
+          style={{
+            marginTop: '6px',
+            transform: `rotate(${rotated ? '180deg' : '0deg'})`,
+            transition: 'transform 200ms linear'
+          }}
+        />
+      </GridItem>
+      <GridItem colSpan={7}>
+        <Collapse in={rotated} animateOpacity>
+          <Box>
+            <Text variant="skill-description" mb={3} pl={isMobile ? 6 : 10}>
+              {text}
+            </Text>
+          </Box>
+        </Collapse>
+      </GridItem>
+    </>
+  )
+}
+
+export default SkillRow
--- pages/skills.js
+++ pages/skills.js
@@ -1,31 +1,59 @@
-import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
+import { Container, Heading, Grid, GridItem } from '@chakra-ui/react'
+import { SiRedwoodjs } from 'react-icons/si'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
-import { Progress } from '@chakra-ui/react'
-import { GridItem } from '../components/grid-item'
-import { SkillRow } from '../components/skills'
+import SkillRow from '../components/skillRow'
+import NodeIcon from '../public/icons/icons8-nodejs'
 import ReactIcon from '../public/icons/icons8-react'
+
+const reactText = `My favourite frontend stack due to its simplicity and 1 way binding.
+ Working heavily in React (both on personal and work basis) by developing 
+ multiple web apps using it. I also rate it highly as one to use in a freelance project.
+ Proficient in state management, redux, context, and hooks.
+`
+
+const nodeText = `First backend framework to learn.
+ Mostly used in freelance due to its simple architecture (especially if combined with Express and Mongoose)
+ alongside the npm libraries that provide essential equipment to finish the job.`
+
+const redwoodText = `The Latest full stack framework in my arsenal of stacks.
+ Have high expectations and hopes for it.
+ Experimenting in it for now by developing local projects for it.
+ `
 
 const Skills = () => (
   <Layout title="Posts">
-    <Container>
-      <Heading as="h3" fontSize={20} mb={4} color="purple.300">
-        Skills
+    <Container mt={10}>
+      <Heading as="h3" fontSize={20} mb={4} mt={4} color="purple.300">
+        Tech Stack
       </Heading>
 
       <Section delay={0.1}>
-        <SkillRow>
-          <ReactIcon />
-          <Container display={'inline-block'} width="100%">
-            <Progress
-              value={80}
-              size="sm"
-              colorScheme="whatsapp"
-              borderRadius={4}
-              transitionDuration={1}
-            />
-          </Container>
-        </SkillRow>
+        <Grid
+          templateRows="repeat(2, 1fr)"
+          templateColumns="repeat(7, 1fr)"
+          rowGap={3}
+        >
+          <GridItem colSpan={7} />
+          <SkillRow
+            icon={<ReactIcon />}
+            progressValue={80}
+            progressColor={'cyan'}
+            text={reactText}
+          />
+          <SkillRow
+            icon={<NodeIcon />}
+            progressValue={75}
+            progressColor={'green'}
+            text={nodeText}
+          />
+          <SkillRow
+            icon={<SiRedwoodjs fontSize={55} color="#bf4722" h="55" />}
+            progressValue={40}
+            progressColor={'orange'}
+            text={redwoodText}
+          />
+        </Grid>
       </Section>
     </Container>
   </Layout>
