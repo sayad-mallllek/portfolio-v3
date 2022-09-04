58614a269d317293dda46c5ea20f901f0fcf0c71
sayad-mallllek <bob@khalilelzein.com>
2022-09-05 01:59:20 +0300

    edited pillow logo behaviour

--- components/logo.js
+++ components/logo.js
@@ -12,12 +12,81 @@ const LogoBox = styled.span`
   line-height: 20px;
   padding: 10px;
 
-  svg {
-    transition: 200ms ease;
+  &:hover svg {
+    animation: shake 500ms infinite linear;
   }
 
-  &:hover svg {
-    transform: rotate(-20deg);
+  .sleeping {
+    width: 40px;
+    height: 40px;
+    position: absolute;
+
+    span {
+      position: absolute;
+      left: 10px;
+      opacity: 0;
+      font-size: 20px;
+      animation: sleep 9s infinite linear;
+    }
+
+    span:nth-child(2) {
+      animation-delay: 3s;
+    }
+
+    span:nth-child(3) {
+      animation-delay: 6s;
+    }
+  }
+
+  .sleeping:hover span {
+    animation: paused !important;
+  }
+
+  @keyframes shake {
+    0% {
+      transform: rotate(0deg);
+    }
+
+    25% {
+      transform: rotate(-20deg);
+    }
+
+    50% {
+      transform: rotate(0deg);
+    }
+
+    75% {
+      transform: rotate(20deg);
+    }
+
+    100% {
+      transform: rotate(0deg);
+    }
+  }
+
+  @keyframes sleep {
+    0% {
+      transform: scale(0.3) translate(0, 0);
+      opacity: 0;
+      color: #c62a88;
+    }
+    1% {
+      opacity: 0;
+    }
+    3% {
+      opacity: 1;
+    }
+    50% {
+      transform: scale(0.65) translate(30%, -100%);
+      color: #b794f4;
+    }
+    75% {
+      opacity: 1;
+    }
+    100% {
+      transform: scale(1) translate(60%, -150%);
+      opacity: 0;
+    }
   }
 `
 
@@ -25,8 +94,13 @@ const Logo = () => {
   return (
     <Link href="/" scroll={false}>
       <a>
-        <LogoBox>
+        <LogoBox className="name-container">
           <GiPillow className="head-icon" />
+          <div class="sleeping">
+            <span>z</span>
+            <span>z</span>
+            <span>z</span>
+          </div>
           <Text
             color={useColorModeValue('gray.800', 'whiteAlpha.900')}
             fontFamily='M PLUS Rounded 1c", sans-serif'
